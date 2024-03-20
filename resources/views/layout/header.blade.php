c0b8dcef80a45863e8ba989792fe19d8b2525d81
ashfaq867 <mitf19e032@gmail.com>
2024-03-20 05:56:00 +0500

    project setup

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,145 @@
+<div class="header-bottom">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-5">
+                <div class="logos">
+                    <img src="{{ asset('assets/images/logo.png') }}" alt="">
+                </div>
+            </div>
+            <!-- <div class="col-7">
+                <div class="logo-name">
+                    <h1 class="logo-name-text">ORION LIGHTING</h1>
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <div class="container">
+        <div class="header">
+            <div class="col-md-9 header-left">
+                <div class="top-nav">
+                    <ul class="memenu skyblue">
+                        <li class="active"><a href="/">Home</a></li>
+                        <li class="grid"><a href="#">Products</a>
+                            <div class="mepanel">
+                                <div class="row">
+                                    <div class="col me-one">
+                                        <h4>LED BULB</h4>
+                                        <ul>
+                                            <li><a href="#">Bulb 5W</a></li>
+                                            <li><a href="#">Bulb 13W</a></li>
+                                            <li><a href="#">Bulb 18W</a></li>
+                                            <li><a href="#">Bulb 28W</a></li>
+                                            <li><a href="#">Bulb 40W</a></li>
+                                            <li><a href="#">Bulb 50W</a></li>
+                                            <li><a href="#">Bulb 65W</a></li>
+                                            <li><a href="#">Bulb 75W</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Led Downlight</h4>
+                                        <ul>
+                                            <li><a href="#">Down Light 7W</a></li>
+                                            <li><a href="#">Down Light 12W</a></li>
+                                            <li><a href="#">Down Light 13W</a></li>
+                                            <li><a href="#">Down Light 18W</a></li>
+                                            <li><a href="#">Down Light 22W</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Batten Light</h4>
+                                        <ul>
+                                            <li><a href="#">Batten Light 40W</a></li>
+                                            <li><a href="#">Batten Light 60W</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Panel Light</h4>
+                                        <ul>
+                                            <li><a href="#">Panel Light 12W</a></li>
+                                            <li><a href="#">Down Light 22W</a></li>
+                                            <li><a href="#">Down Light 40W</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Ice Downligh</h4>
+                                        <ul>
+                                            <li><a href="#">Ice Downlight 12W</a></li>
+                                            <li><a href="#">Ice Downlight 18</a></li>
+                                            <li><a href="#">Ice Downlight 28W</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col me-one">
+                                        <h4>Flood Light</h4>
+                                        <ul>
+                                            <li><a href="#">Flood Light 30W</a></li>
+                                            <li><a href="#">Flood Light 50W</a></li>
+                                            <li><a href="#">Flood Light 100W</a></li>
+                                            <li><a href="#">Flood Light 200W</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="grid"><a href="#">Projects</a>
+                            <div class="mepanel">
+                                <div class="row">
+                                    <div class="col me-one">
+                                        <h4>Shop</h4>
+                                        <ul>
+                                            <li><a href="products.html">LED Bulb</a></li>
+                                            <li><a href="products.html">Street Lights</a></li>
+                                            <li><a href="products.html">Lamp</a></li>
+                                            <li><a href="products.html">Study Lamp</a></li>
+                                            <li><a href="products.html">Solar Lamp</a></li>
+                                            <li><a href="products.html">LED</a></li>
+                                            <li><a href="products.html">Energy Saver</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Shop</h4>
+                                        <ul>
+                                            <li><a href="products.html">LED Bulb</a></li>
+                                            <li><a href="products.html">Street Lights</a></li>
+                                            <li><a href="products.html">Lamp</a></li>
+                                            <li><a href="products.html">Study Lamp</a></li>
+                                            <li><a href="products.html">Solar Lamp</a></li>
+                                            <li><a href="products.html">LED</a></li>
+                                            <li><a href="products.html">Energy Saver</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col me-one">
+                                        <h4>Shop</h4>
+                                        <ul>
+                                            <li><a href="products.html">LED Bulb</a></li>
+                                            <li><a href="products.html">Street Lights</a></li>
+                                            <li><a href="products.html">Lamp</a></li>
+                                            <li><a href="products.html">Study Lamp</a></li>
+                                            <li><a href="products.html">Solar Lamp</a></li>
+                                            <li><a href="products.html">LED</a></li>
+                                            <li><a href="products.html">Energy Saver</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="grid"><a href="{{ route('about') }}">About</a>
+
+                        </li>
+                        <li class="grid"><a href="contact.html">Support</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+            <div class="col-md-3 header-right">
+                <div class="search-bar">
+                    <input type="text" value="Search" onfocus="this.value = '';"
+                        onblur="if (this.value == '') {this.value = 'Search';}">
+                    <input type="submit" value="">
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.master')
+@section('content')
+@include('partials.banner')
+@include('partials.products')
+@include('partials.timeline')
+@endsection
