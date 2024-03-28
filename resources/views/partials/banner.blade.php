a6d27e36bf1c2b4145b4afc07eed412bcbbdf85a
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-03-29 01:12:33 +0500

    home page update

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,24 +1,50 @@
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="2000">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{ asset('assets/images/banner2.png') }}" class="d-block w-50 m-auto" alt="...">
-        </div>
-        <div class="carousel-item text-center">
-            <img src="{{ asset('assets/images/zigzag1.jpeg') }}" class="d-block w-25 m-auto" alt="...">
-        </div>
-        <div class="carousel-item text-center">
-            <img src="{{ asset('assets/images/zigzag2.jpeg') }}" class="d-block w-25 m-auto" alt="...">
-        </div>
-        <div class="carousel-item text-center">
-            <img src="{{ asset('assets/images/zigzag3.png') }}" class="d-block w-25 m-auto" alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+ <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+     <div class="carousel-inner">
+         <div class="carousel-item active" style="background-image: url('{{ asset('assets/images/slider2.jpg') }}')">
+             <div class="backdrop"></div>
+             <div class="carousel-caption">
+                 <div class="carousel-caption-left">
+                     <h1>First slide label</h1>
+                     <p>Some representative placeholder content for the first slide.</p>
+                 </div>
+                 <div class="carousel-caption-right">
+                     <img src="{{ asset('assets/images/orion-13w.jpg') }}" alt="Product 1">
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item" style="background-image: url('{{ asset('assets/images/slider1.jpeg') }}')">
+             <div class="backdrop"></div>
+             <div class="carousel-caption">
+                 <div class="carousel-caption-left">
+                     <h1>First slide label</h1>
+                     <p>Some representative placeholder content for the first slide.</p>
+                 </div>
+                 <div class="carousel-caption-right">
+                     <img src="{{ asset('assets/images/orion-30W-t-shape.jpg') }}" alt="Product 2">
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item" style="background-image: url('{{ asset('assets/images/slider3.jpeg') }}')">
+             <div class="backdrop"></div>
+             <div class="carousel-caption">
+                 <div class="carousel-caption-left">
+                     <h1>First slide label</h1>
+                     <p>Some representative placeholder content for the first slide.</p>
+                 </div>
+                 <div class="carousel-caption-right">
+                     <img src="{{ asset('assets/images/orion-40W.jpg') }}" alt="Product 2">
+                 </div>
+             </div>
+         </div>
+     </div>
+     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+         <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
+         <img src="{{ asset('assets/images/left.svg') }}" alt="">
+         <span class="visually-hidden">Previous</span>
+     </button>
+     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+         <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
+         <img src="{{ asset('assets/images/left.svg') }}" alt="" class="right-svg">
+         <span class="visually-hidden">Next</span>
+     </button>
+ </div>
--- resources/views/partials/products.blade.php
+++ resources/views/partials/products.blade.php
@@ -1,117 +1,51 @@
-<!--product-starts-->
-<div class="product">
-    <div class="container">
-        <div class="product-top">
-            <div class="product-one">
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/orion-13w.jpg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Orion LED Bulb</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product') }}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/orion-30W-t-shape.jpg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Orion LED Bulb</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/orion-40W.jpg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Orion LED Bulb</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-{{--                        <div class="srch">--}}
-{{--                            <span>-50%</span>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/street-light.png') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Street Light</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-{{--                        <div class="srch">--}}
-{{--                            <span>-50%</span>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="product-one">
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/flat-panel.jpeg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Flat Panel Light</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-{{--                        <div class="srch">--}}
-{{--                            <span>-50%</span>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/13watt-downlight.jpg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Downlight</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/panel-light.png') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Panel Light</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-{{--                        <div class="srch">--}}
-{{--                            <span>-50%</span>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-                <div class="col-md-3 product-left">
-                    <div class="product-main simpleCart_shelfItem">
-                        <a href="{{ route('single-product')}}" class="mask"><img class="img-responsive zoom-img"
-                                src="{{ asset('assets/images/orion-batten-llight.jpg') }}" alt="" /></a>
-                        <div class="product-bottom">
-                            <h3>Batten Light</h3>
-                            <p>Explore Now</p>
-
-                        </div>
-{{--                        <div class="srch">--}}
-{{--                            <span>-50%</span>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
+<div class="product-container">
+    <div class="product">
+        <div class="product-inner">
+            <a href="{{ route('single-product')}}">
+                <h2>Orion LED Bulb</h2>
+                <img src="{{ asset('assets/images/orion-13w.jpg') }}" alt="Product 1">
+                <p class="text-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore molestias culpa
+                    quam! Id
+                    quibusdam ducimus est blanditiis facilis distinctio nemo.</p>
+            </a>
         </div>
     </div>
+    <div class="product">
+        <div class="product-inner product-end">
+            <a href="{{ route('single-product')}}">
+                <h2>Orion LED Bulb</h2>
+                <img src="{{ asset('assets/images/orion-30W-t-shape.jpg') }}" alt="Product 2">
+                <p class="text-left">Description of Product 2</p>
+            </a>
+        </div>
+    </div>
+    <div class="product">
+        <div class="product-inner">
+            <a href="{{ route('single-product')}}">
+                <h2>Orion LED Bulb</h2>
+                <img src="{{ asset('assets/images/orion-40W.jpg') }}" alt="Product 2">
+                <p class="text-left">Description of Product 2</p>
+            </a>
+        </div>
+    </div>
+    <div class="product">
+        <div class="product-inner product-end">
+            <a href="{{ route('single-product')}}">
+                <h2>Orion LED Bulb</h2>
+                <img src="{{ asset('assets/images/street-light.png') }}" alt="Product 2">
+                <p class="text-left">Description of Product 2</p>
+            </a>
+        </div>
+    </div>
+    <div class="product">
+        <div class="product-inner">
+            <a href="{{ route('single-product')}}">
+                <h2>Orion LED Bulb</h2>
+                <img src="{{ asset('assets/images/orion-batten-llight.jpg') }}" alt="Product 2">
+                <p class="text-left">Description of Product 2</p>
+            </a>
+        </div>
+    </div>
+
+    <!-- Add more product divs here -->
 </div>
-<!--product-end-->
