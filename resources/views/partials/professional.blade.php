fb8166d23c6a6f0ebce0b3dcb89053ee55ed9d36
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-05-09 23:15:48 +0500

    sub product design

---
+++ resources/views/partials/professional.blade.php
@@ -0,0 +1,113 @@
+@extends('layout.master')
+@section('content')
+
+
+<div class="row p-md-5 gx-0">
+    <div class="col-lg-12 p-5">
+        <h1 class="consumer-heading">Professional</h1>
+        <div class="row flex-wrap justify-content-start">
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Street Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Flood Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Highbay Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Panel Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED T Series Bulbs</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Spot Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Tube Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Stadium Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Solar Street Lights</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 col-6">
+                <div class="consumer-sub-content">
+                    <a href="{{ route('product-list') }}">
+                        <div class="consumer-sub-img">
+                            <img src="{{ asset('assets/images/indoor1.jpeg') }}" alt="">
+                        </div>
+                        <h3 class="consumer-sub-link">LED Smart Lighting</h3>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
